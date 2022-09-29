f41cb49bda5ea6c64f2e1c4571818f12b4fa4c66
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2022-09-29 13:21:23 -0400

    followed read me

---
+++ cli.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+
+//imports
+import minimist from 'minimist';
+import moment from "moment-timezone";
+import fetch from "node-fetch";
+
+const args = minimist(process.argv.slice(2));
+
+if (args.h) {
+	printHelp();
+	process.exit(0);
+}
+
+function printHelp() { //helper function
+	console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE")
+	console.log( "-h           Show this help message and exit.")
+	console.log("-n, -s        Latitude: N positive; S negative.")
+	console.log("-e, -w        Longitude: E positive; W negative.")
+	console.log("-z            Time zone: uses tz.guess() from moment-timezone by default.")
+	console.log("-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.")
+	console.log("-j            Echo pretty JSON from open-meteo API and exit.")
+}//end help
+
+let d = args.d;
+let latitude;
+let longitude; 
+
+if(args.n && !args.s){
+	latitude = args.n;
+} else if(args.s && !args.n){
+	latitude = -1 * args.s;
+} else {
+	console.log("Must use -n or -s to speciy the latitude, -h for help");
+	process.exit(1);
+}
+
+if(args.e && !args.w){
+	longitude = args.e;
+} else if(args.w && !args.e){
+	longitude = -1 * args.w;
+} else {
+	console.log("Must use -e or -w to speciy the longitude, -h for help");
+	process.exit(1);
+}
+
+let timezone =  args.z ? args.z : moment.tz.guess();
+//use tz.guest by default
+
+// Make a request
+//latitude and longitude constants
+
+//fetch API call
+
+const base_url = "https://api.open-meteo.com/v1/forecast"
+
+const data_string = "latitude=" + latitude + "&longitude=" + longitude +"&daily=precipitation_sum&timezone=" + timezone
+
+const url = base_url + "?" + data_string
+
+const response = await fetch(url);
+// Get the data from the request
+const data = await response.json();
+
+if(data.error) {
+	console.log(data.reason + "\n");
+	process.exit(1);
+}else if(args.j) {
+	console.log(data);
+    process.exit(0);
+}
+
+const days = args.d 
+
+// Below, we've defined days corresponding to the command line argument -d. We'll assume args is defined from minimist's output, which is the parsed command line arguments.
+
+if (days == 0) {
+  console.log("today.")
+} else if (days > 1) {
+  console.log("in " + days + " days.")
+} else {
+  console.log("tomorrow.")
+}
+
+console.log(data);
+process.exit(0);
